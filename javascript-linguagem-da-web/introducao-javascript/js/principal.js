7807b22bba91b396da93f670c635c8f154b56d75
Pollyana Minatel <pollyminatel@gmail.com>
2023-03-28 23:08:29 -0300

    Aula 4

--- javascript-linguagem-da-web/introducao-javascript/js/principal.js
+++ javascript-linguagem-da-web/introducao-javascript/js/principal.js
@@ -4,7 +4,6 @@ titulo.textContent = "Aparecida Nutricionista";
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i=0; i < pacientes.length; i++){
-    console.log(pacientes[i])
     var paciente = pacientes[i];
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
@@ -34,6 +33,40 @@ for(var i=0; i < pacientes.length; i++){
         var imc = peso / (altura * altura) ; 
         tdImc.textContent = imc.toFixed(2);
     }
-};
+}
 
+// titulo.addEventListener("click", function() {
+//     console.log("Olá, eu fui clicado");
+// });
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("Olá, eu fui clicado");
+
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
