03a50be845b324a7a28283144a7c41fa0bad5ff5
Pollyana Minatel <pollyminatel@gmail.com>
2023-04-26 14:16:57 -0300

    Aula 5

--- jquery1/public/js/main.js
+++ jquery1/public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reniciaJogo);
 });
 
@@ -37,13 +38,32 @@ function inicializaCronometro(){
             if(tempoRestante < 1){
                 campo.attr("disabled",true);
                 clearInterval(cronometroId);
+                campo.addClass("campo-desativado");
                 $("#botao-reiniciar").attr("disabled", false);
             }
         },1000);
     });
 };
 
+function inicializaMarcadores(){
+    var frase = $('.frase').text();
+    campo.on('input', function(){
+        var digitado = campo.val();
+    var comparavel = frase.substr(0,digitado.length);
+    if(digitado == comparavel) {
+        campo.addClass('borda-verde');
+        campo.removeClass('borda-vermelha');
+    }else{
+        campo.addClass('borda-vermelha');
+        campo.removeClass('borda-verde')
+    };
+    });
+}
+
 function reniciaJogo(){    
+    campo.removeClass('campo-desativado');
+    campo.removeClass('borda-verde');
+    campo.removeClass('borda-vermelha');
     campo.attr("disabled", false);
     campo.val("");
     $("#contador-palavras").text('0');
