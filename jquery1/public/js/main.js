ea6ced436edf8721fb67180632d60ca97064ea9d
Pollyana Minatel <pollyminatel@gmail.com>
2023-04-30 18:23:19 -0300

    Aula 6

--- jquery1/public/js/main.js
+++ jquery1/public/js/main.js
@@ -7,12 +7,15 @@ $(function(){
     inicializaCronometro();
     inicializaMarcadores();
     $("#botao-reiniciar").click(reniciaJogo);
+    $(".botao-remover").click(function(event){
+        event.preventDefault();
+        $(this).parent().parent().remove();
+    })
 });
 
 function atualizaTamanhoFrase(){
     var frase = $(".frase").text();
     var numPalavras = frase.split(" ").length;
-    
     var tamanhoFrase = $("#tamanho-frase");
     tamanhoFrase.text(numPalavras);
 };
@@ -22,7 +25,6 @@ function inicializaContadores(){
         var conteudo = campo.val();
         var qtdPalavras = conteudo.split(/\S+/).length -1;
         $("#contador-palavras").text(qtdPalavras);
-        
         var qtdCaracteres = conteudo.length;
         $("#contador-caracteres").text(qtdCaracteres);
     });
@@ -36,38 +38,46 @@ function inicializaCronometro(){
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
             if(tempoRestante < 1){
-                campo.attr("disabled",true);
                 clearInterval(cronometroId);
-                campo.addClass("campo-desativado");
-                $("#botao-reiniciar").attr("disabled", false);
+                finalizaJogo();
             }
         },1000);
     });
 };
 
-function inicializaMarcadores(){
-    var frase = $('.frase').text();
-    campo.on('input', function(){
+function finalizaJogo(){
+    campo.attr("disabled",true);
+    campo.toggleClass("campo-desativado");
+    $("#botao-reiniciar").attr("disabled",false);
+    inserePlacar();
+};
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
         var digitado = campo.val();
-    var comparavel = frase.substr(0,digitado.length);
-    if(digitado == comparavel) {
-        campo.addClass('borda-verde');
-        campo.removeClass('borda-vermelha');
-    }else{
-        campo.addClass('borda-vermelha');
-        campo.removeClass('borda-verde')
-    };
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
     });
 }
 
+
+
 function reniciaJogo(){    
-    campo.removeClass('campo-desativado');
-    campo.removeClass('borda-verde');
-    campo.removeClass('borda-vermelha');
     campo.attr("disabled", false);
     campo.val("");
     $("#contador-palavras").text('0');
     $("#contador-caracteres").text('0');
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde")
 };
