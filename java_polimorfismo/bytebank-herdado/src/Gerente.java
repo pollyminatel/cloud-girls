c7803fe37d826202f187ecd4f39cbf7b81684c05
Pollyana Minatel <pollyminatel@gmail.com>
2023-05-08 21:48:47 -0300

    Aula 7

--- java_polimorfismo/bytebank-herdado/src/Gerente.java
+++ java_polimorfismo/bytebank-herdado/src/Gerente.java
@@ -1,6 +1,10 @@
 public class Gerente extends Funcionario implements Autenticavel{
 	
-	private int senha;
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	public double getBonificacao() {
 		return super.getSalario();
@@ -8,14 +12,11 @@ public class Gerente extends Funcionario implements Autenticavel{
 	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		}
-		return false;
+		return this.autenticador.autentica(senha);
 	}
 }
