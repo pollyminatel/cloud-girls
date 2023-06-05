408abab7ac1ed60e23811b57e30b672c98da1c25
Pollyana Minatel <pollyminatel@gmail.com>
2023-06-05 18:37:21 -0300

    Aula 1 e 2

---
+++ java_lambda/java_lambda/bytebank-herdado-conta/src/banco/modelo/CalculadorDeImposto.java
@@ -0,0 +1,17 @@
+package banco.modelo; 
+import banco.modelo.*;
+
+public class CalculadorDeImposto {
+	
+	private double totalImposto;
+	
+	public void registra(Tributavel t) {
+		double valor = t.getValorImposto();
+		this.totalImposto += valor;
+	}
+	
+	public double getTotalImposto() {
+		return totalImposto;
+	}
+
+}
